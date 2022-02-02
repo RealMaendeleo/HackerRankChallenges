0815cf7a19fc95064728d0a622dfa5aa3cec26eb
Vlad Karmanchikov <vlad.karmanchikofff@gmail.com>
2022-02-02 23:15:10 +0300

    Initial commit

---
+++ src/DataStructures/ArrayList.java
@@ -0,0 +1,32 @@
+package DataStructures;
+
+import java.util.*;
+
+public class ArrayList {
+
+    public static void main(String[] args) {
+        List<List<Integer>> matrix = new java.util.ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int linesNumber = sc.nextInt();
+        int numbersCount;
+        for (int i = 0; i < linesNumber; i++) {
+            numbersCount = sc.nextInt();
+            List<Integer> numbersLine = new java.util.ArrayList<>(numbersCount);
+            for (int j = 0; j < numbersCount; j++) {
+                numbersLine.add(sc.nextInt());
+            }
+            matrix.add(numbersLine);
+        }
+        int queriesCount = sc.nextInt();
+        int row, column;
+        for (int i = 0; i < queriesCount; i++) {
+            row = sc.nextInt();
+            column = sc.nextInt();
+            try {
+                System.out.println(matrix.get(row).get(column));
+            } catch (IndexOutOfBoundsException exc) {
+                System.out.println("ERROR!");
+            }
+        }
+    }
+}
---
+++ src/DataStructures/Lists.java
@@ -0,0 +1,28 @@
+package DataStructures;
+
+import java.util.*;
+import java.util.ArrayList;
+
+public class Lists {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            numbers.add(sc.nextInt());
+        }
+        int queriesNumber = sc.nextInt();
+        String command;
+        for (int i = 0; i < queriesNumber; i++) {
+            command = sc.next();
+            if (command.equals("Insert")) {
+                numbers.add(sc.nextInt(), sc.nextInt());
+            } else if (command.equals("Delete")) {
+                numbers.remove(sc.nextInt());
+            }
+        }
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
